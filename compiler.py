8aa141165b50cbc09111e29d5fa7eccd08d3b3c0
Hamila Mailee <hamilamailee77@gmail.com>
2022-12-01 19:40:19 +0330

    Initial project files and Testcases added

---
+++ compiler.py
@@ -0,0 +1,8 @@
+from scanner import Scanner
+
+""" Hamila Mailee       97106295 """
+""" Sepehr Ilami        """
+""" 
+    References:
+        [1]
+"""
---
+++ scanner.py
@@ -0,0 +1,9 @@
+class Scanner:
+    def __init__(self) -> None:
+        input_file = "input.txt"
+        KEYWORD = ["if", "else", "void", "int", "while", "break",
+                   "switch", "default", "case", "return", "endif"]
+        WHITESPACE = [" ", "\n", "\r", "\t", "\v", "\f"]
+
+    def get_next_token():
+        return None
