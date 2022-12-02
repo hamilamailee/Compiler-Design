1c204d4bf1d811b280e466403dc8d6b7f582d0d3
Hamila Mailee <hamilamailee77@gmail.com>
2022-12-02 04:05:03 +0330

    Comment with // handled

--- scanner.py
+++ scanner.py
@@ -6,8 +6,9 @@ class Scanner:
         self.KEYWORD = ["if", "else", "void", "int", "while", "break",
                         "switch", "default", "case", "return", "endif"]
         self.SYMBOL = [";", ":", ",", "[", "]",
-                       "(", ")", "{", "}", "+", "-", "*", "<", "/"]
+                       "(", ")", "{", "}", "+", "-", "<"]
         self.WHITESPACE = [" ", "\n", "\r", "\t", "\v", "\f"]
+        self.EXTRA = ["=", "*", "/"]
 
         self.lines = open(input_file).readlines()
         self.start = 0
@@ -15,10 +16,12 @@ class Scanner:
         self.state = 0
 
     def tokenize(self):
+        line_index = 0
         for line in self.lines:
-            self.get_next_token(line.strip())
+            self.get_next_token(line.strip(), line_index)
+            line_index += 1
 
-    def get_next_token(self, line):
+    def get_next_token(self, line, line_index):
         tokens = []
         errors = []
         while self.pointer != len(line):
@@ -38,18 +41,16 @@ class Scanner:
                     self.state = 6
                 elif char == "*":
                     self.state = 8
-                    self.start = self.pointer
                 elif char == "/":
                     self.state = 10
-                    self.start = self.pointer
                 elif char in self.WHITESPACE:
                     pass
                 else:
-                    errors.append("({}, Invalid input)")
+                    errors.append("({}, Invalid input)".format(char))
             elif self.state == 1:
                 if char in self.LETTER or char in self.DIGIT:
                     pass
-                else:
+                elif char in (self.WHITESPACE + self.SYMBOL + self.EXTRA):
                     if line[self.start:self.pointer] in self.KEYWORD:
                         tokens.append("(KEYWORD, {})".format(
                             line[self.start:self.pointer]))
@@ -58,6 +59,10 @@ class Scanner:
                             line[self.start:self.pointer]))
                     self.state = 0
                     continue
+                else:
+                    errors.append("({}, Invalid input)".format(
+                        line[self.start:self.pointer+1]))
+                    self.state = 0
             elif self.state == 3:
                 if char in self.DIGIT:
                     pass
@@ -67,7 +72,7 @@ class Scanner:
                     self.state = 0
                     continue
                 else:
-                    errors.append("({}, Invaid number)".format(
+                    errors.append("({}, Invalid number)".format(
                         line[self.start:self.pointer+1]))
                     self.state = 0
             elif self.state == 6:
@@ -77,12 +82,38 @@ class Scanner:
                 else:
                     tokens.append("(SYMBOL, =)")
                     continue
+            elif self.state == 8:
+                self.state = 0
+                if char == "/":
+                    errors.append("(*/, Unmatched comment)")
+                else:
+                    tokens.append("(SYMBOL, *)")
+                    continue
+            elif self.state == 10:
+                if char == "/":
+                    self.state = 11
+                    self.start = self.pointer - 1
+                elif char == "*":
+                    self.state = 13
+                    self.start = self.pointer - 1
+                else:
+                    tokens.append("(SYMBOL, /)")
+                    self.state = 0
+                    continue
+            elif self.state == 11:
+                if self.pointer != len(line) - 1:
+                    pass
+                else:
+                    # tokens.append("(COMMENT,{})".format(
+                    #     line[self.start:self.pointer+1]))
+                    self.state = 0
             self.pointer += 1
-        print("\nTOKENS:")
-        for t in tokens:
-            print(t, end=" ")
-        print("\nERRORS:")
-        for e in errors:
-            print(e, end="")
+        if len(tokens) > 0:
+            print("\nTOKENS:")
+            for t in tokens:
+                print(t, end=" ")
+            print("\nERRORS:")
+            for e in errors:
+                print(e, end=" ")
         self.pointer = 0
         return None
