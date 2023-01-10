375354f9c56f13ef95e92e248a6214a0a123f521
Hamila Mailee <hamilamailee77@gmail.com>
2023-01-10 21:38:48 +0330

    Initial debug of panic mode section

--- lrparser.py
+++ lrparser.py
@@ -17,7 +17,7 @@ class Parser:
 
         self.stack_state = ["0"]
         self.stack_token = []
-        self.token = None
+        self.token = [None, None]
         self.action = "shift"
 
         self.root = None
@@ -28,13 +28,15 @@ class Parser:
     def parse(self):
         while (self.action != "accept"):
 
-            if self.token is None:
+            if self.token[0] is None:
                 token, string = self.scanner.get_next_token()
-                self.token = string if token in [
+                self.token = [token, string]
+                self.token[0] = string if token in [
                     "KEYWORD", "SYMBOL"] else token
 
             try:
-                self.action = self.parse_table[self.stack_state[-1]][self.token]
+                self.action = self.parse_table[self.stack_state[-1]
+                                               ][self.token[0]]
             except:
                 self.action = "PANIC"
 
@@ -44,7 +46,7 @@ class Parser:
             elif self.action.startswith("shift"):
                 self.update_stack(self.token, Node(
                     "({}, {})".format(token, string)))
-                self.token = "$" if self.token == "$" else None
+                self.token[0] = "$" if self.token[0] == "$" else None
 
             elif self.action.startswith("reduce"):
                 production_rule = self.grammar[self.action.split("_")[1]]
@@ -67,19 +69,37 @@ class Parser:
 
                 self.action = self.parse_table[self.stack_state[-1]
                                                ][production_rule[0]]
-                self.update_stack(production_rule[0], self.root)
+                self.update_stack([production_rule[0], None], self.root)
 
             else:
                 self.error = True
                 # TODO: PANIC MODE IMPLEMENTATION
-                print(f'#line_number : syntax error , illegal {self.token}')
-                self.stack_state.pop()
-                self.stack_state.pop()
-                while("goto" not in value for value in self.parse_table[self.stack_state[-1]].values()):
+                print(
+                    f'#{self.scanner.line_index + 1} : syntax error , illegal {self.token[0]}')
+                token, string = self.scanner.get_next_token()
+                self.token = [token, string]
+                self.token[0] = string if token in [
+                    "KEYWORD", "SYMBOL"] else token
+
+                while(not any(value.startswith("goto") for value in list(self.parse_table[self.stack_state[-1]].values()))):
+                    print(
+                        list(self.parse_table[self.stack_state[-1]].values()))
+                    print(
+                        f'syntax error , discarded ({self.get_tok(self.token)}, {self.token[1]}) from stack')
                     self.stack_state.pop()
-                    self.stack_state.pop()
-                error_handling = self.parse_table[self.stack_state[-1]
-                                                  ][self.stack_state[-2]]
+                    self.stack_token.pop()
+                while True:
+                    try:
+                        error_handling = self.parse_table[self.stack_state[-1]
+                                                          ][self.token[0]]
+                        break
+                    except:
+                        print(
+                            f'#{self.scanner.line_index + 1} : syntax error , discarded {self.token[0]} from input')
+                        token, string = self.scanner.get_next_token()
+                        self.token = string if token in [
+                            "KEYWORD", "SYMBOL"] else token
+
                 ac, ind = error_handling.split("_")
                 list_of_gotos = []
                 for (key, value) in self.parse_table[self.stack_state[-1]]:
@@ -105,6 +125,13 @@ class Parser:
                 return
 
         self.write_files()
+
+    def get_tok(self, token):
+        if token[1] in self.scanner.KEYWORD:
+            return "KEYWORD"
+        elif token[1] in (self.scanner.SYMBOL + self.scanner.EXTRA + ["=="]):
+            return "SYMBOL"
+        return token[0]
 
     def update_stack(self, token, node: Node):
         if self.action == "accept":
