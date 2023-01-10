80089b43d75fcb2d08cf7e2eb557908e6baf5d59
Hamila Mailee <hamilamailee77@gmail.com>
2023-01-10 12:02:39 +0330

    initial version of completed LR(1) - No Panic Mode

--- lrparser.py
+++ lrparser.py
@@ -23,33 +23,29 @@ class Parser:
         self.reductions = []
         self.strings = []
 
+        self.error = False
+
     def parse(self):
         while (self.action != "accept"):
-            # print("state stack:\t", self.stack_state)
-            # print("string stack:\t", self.stack_token)
-
             if self.token is None:
                 try:
                     self.token, string = self.scanner.get_next_token()
-                    # print("token:", self.token, "string:", string)
+                    self.strings.append("({}, {})".format(self.token, string))
                     if self.token in ["KEYWORD", "SYMBOL"]:
                         self.token = string
                 except:
                     self.token = "$"
-            # print("input:\t", self.token)
             action = self.parse_table[self.stack_state[-1]][self.token]
             if action == "accept":
                 self.action = action
                 continue
             self.action, index = action.split("_")
-            # print("action:", self.action, "state:", index)
             if self.action == "shift":
                 self.stack_token.append(self.token)
                 self.stack_state.append(index)
                 self.token = None
             elif self.action == "reduce":
                 production_rule = self.grammar[index]
-                # print("Production Rule:\t", production_rule)
                 if production_rule[-1] != 'epsilon':
                     self.stack_token = self.stack_token[:2 -
                                                         len(production_rule)]
@@ -66,14 +62,39 @@ class Parser:
                 self.action, index = action.split("_")
                 self.stack_token.append(production_rule[0])
                 self.stack_state.append(index)
-                # print("action index", index, self.action)
-                # print("NEW state stack:\t", self.stack_state)
-                # print("NEW string stack:\t", self.stack_token)
             else:
-                break
-            # print("=====================================================")
-        self.generate_parse_tree()
+                self.error = True
 
-    def generate_parse_tree(self):
-        for i in self.reductions:
-            print(i)
+        self.write_files()
+
+    def generate_parse_tree(self, root_node, root_children):
+        children = dict()
+        for c in root_children[2:]:
+            children[c] = Node(c, parent=root_node)
+
+        for c in list(children)[::-1]:
+            if len(self.reductions) == 0:
+                return
+            if self.reductions[-1][0] == c:
+                self.generate_parse_tree(children[c], self.reductions.pop())
+                children.pop(c)
+
+    def write_files(self):
+        root = Node(self.reductions[-1][0])
+        self.generate_parse_tree(root, self.reductions.pop())
+        end = Node("$", parent=root)
+
+        self.strings.pop()
+        for l in root.leaves[::-1][1:]:
+            if l.name != "epsilon":
+                l.name = self.strings.pop()
+
+        with open('parse_tree.txt', 'a',  encoding='utf-8') as f:
+            for pre, fill, node in RenderTree(root):
+                f.write("%s%s\n" % (pre, node.name))
+            f.close()
+
+        with open('syntax_erros.txt', 'a', encoding='utf-8') as f:
+            if not self.error:
+                f.write("There is no syntax error.")
+            f.close()
